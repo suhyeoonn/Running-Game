3f1c6225233e27663b08ecc0ab8e4a2440b424e9
suhyeoonn <suhyeon1740@gmail.com>
2023-06-11 17:18:57 +0900

    타일맵 적용 및 카메라 이동 구현

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float cameraSpeed = 5.0f;
+
+    public GameObject player;
+
+    private void Update()
+    {
+        Vector3 dir = player.transform.position - this.transform.position;
+        Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, 0.0f, 0.0f);
+        this.transform.Translate(moveVector);
+    }
+}
